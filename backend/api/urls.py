6c0dbc3887a282465f67b36ab08f011bd05db80d
anthonynarine <anarine83@gmail.com>
2023-04-01 22:03:10 -0400

    View and path set up to get an single product by id:

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.helloWorld, name="Julia"), 
-    # path("", views.getRoutes, name="routes"), 
-    path ("products/", views.getProducts, name="products")
+    path("", views.helloWorld, name="TEST"), 
+    path("routes/", views.getRoutes, name="routes"), 
+    path ("products/", views.getProducts, name="products"),
+    path ("products/<str:pk>/", views.getProduct, name="product")
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .products import products
 # Create your views here.
 
+@api_view(['Get'])
 def helloWorld(request):
-    return JsonResponse("Hello world", safe=False)
+    return Response("testing view-url config")
 
+
+@api_view(['Get'])
+def getRoutes(request):
+    routes = [...
+    ]
+    return Response(routes)
+
+# function view to get all products
+@api_view(['Get'])
 def getProducts(request):
-    return JsonResponse(products, safe=False)
+    return Response(products)
 
-
-# def getRoutes(request):
-#     return JsonResponse("Hello", safe=False)
-
+# function view to get a product by id
+@api_view(['Get'])
+def getProduct(request, pk):
+    product = None
+    for item in products:
+        if item["_id"] == pk:
+            product = item
+            break
+    
+    return Response(product)
