79c505c694cdc90c7031fbe03b75b78700492b0f
anthonynarine <anarine83@gmail.com>
2023-04-08 11:32:29 -0400

    pillow installed to handle image processing + migrations made

--- backend/api/models.py
+++ backend/api/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
 # on_delete=models.SET_NULL allow a user to be deleted but their products will not
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    # image =
+    image = models.ImageField(null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
